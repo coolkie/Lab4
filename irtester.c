b1910fb3d5489bbadbf4817dba4d5c33cb9d9707
coolkie <43559520+coolkie@users.noreply.github.com>
2018-10-13 01:22:29 -0400

    Add files via upload

--- irtester.c
+++ irtester.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-#include <wiringPi.h>
+#include <WiringPi.h>
 
 int main(int argc, char *argv[])
 {
-  int i;
-  wiringPiSetup () ;
-  pinMode(0, INPUT);
-  while(1) {
-    printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
-    printf("Waiting for event\n");
-    while(digitalRead(0) == 0);
-    printf("Alarm\n");
-  }
-  /*NOTREACHED*/
-  return 0 ;
+    wiringPiSetup () ;
+    pinMode(0, INPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    while(1) {
+        digitalWrite(1,LOW);
+        digitalWrite(2,LOW);
+        printf("Waiting for reset\n");
+        while(digitalRead(0) == 1);
+        digitalWrite(1, LOW);
+        digitalWrite(2, HIGH);
+        delay(50);
+        digitalWrite(2, LOW);
+        /*ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "jl$
+         */printf("Waiting for event\n");
+        while(digitalRead(0) == 0);
+        digitalWrite(1,HIGH);
+        digitalWrite(2, LOW);
+        delay(50);
+        digitalWrite(1,LOW);
+        printf("Alarm\n");
+    }
+    /*NOTREACHED*/
+    return 0;
 }
